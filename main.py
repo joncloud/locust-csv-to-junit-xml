3c495e3bd551724a286d8518dda96b22efcfad9a
Jonathan Berube <jdberube@gmail.com>
2019-09-05 07:11:25 -0700

    Tune up based on linting

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
 
-import csv, datetime, getopt, sys
+import csv
+import datetime
+import getopt
+import sys
 import xml.etree.ElementTree as ET
+
 
 def main(argv):
     prefix = ''
 
     try:
-        opts, _ = getopt.getopt(sys.argv[1:], 'hp:',['prefix='])
+        opts, _ = getopt.getopt(sys.argv[1:], 'hp:', ['prefix='])
     except getopt.GetoptError:
         print('main.py -p <prefix>')
         sys.exit(2)
@@ -27,6 +31,7 @@ def main(argv):
     xml_tree = ET.ElementTree(testsuites)
     xml_tree.write("test_results.xml")
 
+
 def create_testsuites():
     testsuites = ET.Element('testsuites')
 
@@ -41,25 +46,28 @@ def create_testsuites():
 
     return (testsuites, testsuite)
 
+
 def append_testcases(prefix, testsuite):
     test_count = 0
     failure_count = 0
 
     with open(prefix + '_requests.csv', mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
-        
+
         line_count = 0
         for row in csv_reader:
             if line_count > 0:
                 testcase = ET.SubElement(testsuite, 'testcase')
-                testcase.set('name', f'{row["Method"]}\t{row["Name"]} Average response time')
+                name = f'{row["Method"]}\t{row["Name"]} Average response time'
+                testcase.set('name', name)
                 test_count += int(row['# requests'])
                 failure_count += int(row['# failures'])
                 testcase.set('time', row['Average response time'])
             line_count += 1
-        
+
         testsuite.set('tests', str(test_count))
         testsuite.set('failures', str(failure_count))
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
